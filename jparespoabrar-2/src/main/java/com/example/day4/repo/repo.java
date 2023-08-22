e9605d1e7e1438e932d8fa59be3db5dd9744ec4c
naveens498 <108707269+naveens498@users.noreply.github.com>
2023-08-22 15:46:38 +0530

    Delete jparespoabrar-2 directory

--- jparespoabrar-2/src/main/java/com/example/day4/repo/repo.java
+++
@@ -1,9 +0,0 @@
-package com.example.day4.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.day4.model.model;
-
-public interface repo extends JpaRepository<model, Integer> {
-
-}
