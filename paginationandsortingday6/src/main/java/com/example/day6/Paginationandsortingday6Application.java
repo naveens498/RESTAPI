cce244e2f087a27008321640f22b4eaba6d434f7
naveens498 <108707269+naveens498@users.noreply.github.com>
2023-08-24 14:30:03 +0530

    Add files via upload

---
+++ paginationandsortingday6/src/main/java/com/example/day6/Paginationandsortingday6Application.java
@@ -0,0 +1,13 @@
+package com.example.day6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Paginationandsortingday6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Paginationandsortingday6Application.class, args);
+	}
+
+}
---
+++ paginationandsortingday6/src/main/java/com/example/day6/repo/rep.java
@@ -0,0 +1,5 @@
+package com.example.day6.repo;
+
+public interface rep {
+
+}
