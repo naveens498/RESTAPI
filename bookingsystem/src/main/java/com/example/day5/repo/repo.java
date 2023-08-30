f4236c121ba5ae818a3cc2f1722614b041e351da
naveens498 <108707269+naveens498@users.noreply.github.com>
2023-08-30 15:49:14 +0530

    Delete bookingsystem directory

--- bookingsystem/src/main/java/com/example/day5/repo/repo.java
+++
@@ -1,9 +0,0 @@
-package com.example.day5.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.day5.model.model;
-
-public interface repo extends JpaRepository<model, Integer>{
-
-}
